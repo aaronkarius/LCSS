4350dd9750797ae9df9d34e8dd0e6b7255a93e07
slime5 <85314060+slime5@users.noreply.github.com>
2024-01-24 22:20:26 -0500

    base outline, making stuff work, updating packages

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,13 +2,17 @@
 import Link from "next/link";
 import React from "react";
 import DarkModeToggle from "./DarkModeToggle";
+import { FaHome } from "react-icons/fa";
 
 const NavBar = () => {
     return (
         <div className="sticky top-0 flex justify-end p-6 bg-slate-300 dark:bg-slate-500">
+            <div>
+                <FaHome size={25} />
+            </div>
             <div className="flex items-center gap-4 ml-auto sm:gap-6">
-                <Link className="navbar-text" href="/">
-                    Test Page
+                <Link className="navbar-text hover:underline" href="/">
+                    Learn More
                 </Link>
                 <DarkModeToggle />
             </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,39 @@
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="flex flex-col items-center justify-center h-full gap-12 p-12">
+            <h1 className="text-xl font-bold tracking-tighter lg:text-3xl">
+                The Longest Common Subsequence and Substring
+            </h1>
+            <div className="flex flex-col gap-8">
+                <div className="flex flex-col gap-4">
+                    <div>
+                        <label htmlFor="firstString">First String</label>
+                        <input
+                            type="text"
+                            id="firstString"
+                            className="input"
+                            placeholder="first"
+                        ></input>
+                    </div>
+                    <div>
+                        <label htmlFor="secondString">Second String</label>
+                        <input
+                            type="text"
+                            id="secondString"
+                            className="input"
+                            placeholder="second"
+                        ></input>
+                    </div>
+                </div>
+                <div className="flex gap-4">
+                    <button className="bg-indigo-600 button dark:bg-indigo-500 dark:hover:bg-indigo-600 hover:bg-indigo-700">
+                        Compute
+                    </button>
+                    <button className="bg-gray-400 button dark:bg-gray-500 dark:hover:bg-gray-600 hover:bg-gray-500">
+                        Clear
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
